4f12efa68fcb22206d3429a6bcde596a4fda14ba
Otniel Marques <otnielmarques3@gmail.com>
2026-01-25 00:13:57 -0300

    CRUD pronto

--- src/main/java/com/registradorSQL/SQLCatalog/controller/ScriptController.java
+++ src/main/java/com/registradorSQL/SQLCatalog/controller/ScriptController.java
@@ -2,12 +2,13 @@ package com.registradorSQL.SQLCatalog.controller;
 
 import com.registradorSQL.SQLCatalog.model.Script;
 import com.registradorSQL.SQLCatalog.service.ScriptService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/scripts")
@@ -19,5 +20,35 @@ public class ScriptController {
     @PostMapping
     public ResponseEntity<Script> criarScript(@RequestBody Script script){
         return ResponseEntity.status(201).body(scriptService.criaScript(script));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Script>> getScript(){
+        return ResponseEntity.status(200).body(scriptService.listarScript());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Script> getScriptById(@PathVariable("id") Long id){
+        if (scriptService.listarScriptById(id) != null){
+            return ResponseEntity.status(200).body(scriptService.listarScriptById(id));
+        }
+        return ResponseEntity.status(404).body(null);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Script> atualizarScript(@RequestBody Script script,
+                                                  @PathVariable("id") Long id){
+        if (scriptService.atualizarScript(script, id) != null){
+            return ResponseEntity.status(200).body(script);
+        }
+        return ResponseEntity.status(404).body(null);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Script> deletarScript(@PathVariable("id") Long id){
+        if (scriptService.deleteScript(id) == 0){
+            return ResponseEntity.status(204).body(null);
+        }
+        return ResponseEntity.status(404).body(null);
     }
 }
--- src/main/java/com/registradorSQL/SQLCatalog/service/ScriptService.java
+++ src/main/java/com/registradorSQL/SQLCatalog/service/ScriptService.java
@@ -5,26 +5,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class ScriptService {
 
-    @Autowired
-    private Script script;
+
+    List<Script> listScript = new ArrayList<>();
 
     private Long proximoId = 1L;
 
     public Script criaScript(Script script){
-        this.script.setId(proximoId++);
-        this.script.setTitulo(script.getTitulo());
-        this.script.setDescricao(script.getDescricao());
-        this.script.setConteudo(script.getConteudo());
-        this.script.setBancoDados(script.getBancoDados());
-        this.script.setCategoria(script.getCategoria());
-        this.script.setTags(script.getTags());
-        this.script.setDataCriacao(LocalDateTime.now());
-        this.script.setDataAtualizacao(LocalDateTime.now());
-        return this.script;
+        script.setId(proximoId++);
+        script.setDataCriacao(LocalDateTime.now());
+        script.setDataAtualizacao(LocalDateTime.now());
+        listScript.add(script);
+        return script;
+    }
+
+    public List<Script> listarScript(){
+        listScript.sort(Comparator.comparing(Script::getDataAtualizacao).reversed());
+        return this.listScript;
+    }
+
+    public Script listarScriptById(Long id){
+        for (Script s : listScript){
+            if (s.getId() == id){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public Script atualizarScript(Script script, Long id){
+            for (int i = 0; i<= listScript.size(); i++){
+                if (listScript.get(i).getId().equals(id)){
+                    script.setId(listScript.get(i).getId());
+                    script.setDataAtualizacao(LocalDateTime.now());
+                    script.setDataCriacao(listScript.get(i).getDataCriacao());
+                    listScript.set(i, script);
+                    return script;
+                }
+        }
+        return null;
+    }
+
+    public Integer deleteScript(Long id){
+        for (Script s : listScript){
+            if (s.getId() == id){
+                listScript.remove(s);
+                return 0;
+            }
+        }
+        return 1;
     }
 }
 
