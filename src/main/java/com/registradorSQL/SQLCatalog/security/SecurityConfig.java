ac44bd22e237e4a940ff613be0852c4736d7cfa2
Otniel Marques <otnielmarques3@gmail.com>
2026-02-14 00:49:02 -0300

    URL frontend adicionada no arquivo CORS

--- src/main/java/com/registradorSQL/SQLCatalog/security/SecurityConfig.java
+++ src/main/java/com/registradorSQL/SQLCatalog/security/SecurityConfig.java
@@ -69,7 +69,8 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://localhost:4200",
                 "http://localhost:5173",
-                "http://127.0.0.1:4200"
+                "http://127.0.0.1:4200",
+                "https://sql-catalog.vercel.app"
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
